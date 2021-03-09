86e523611da32456f2b268cd50ad60f1c9bfafdd
unknown <martin.arpin@outlook.com>
2021-03-08 17:37:52 -0800

    Updates

---
+++ precache-manifest.47a09de78051d6d529e18e337b9ae3ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854278244e4a46aff83b769f142f374d",
+    "url": "/cv_project/index.html"
+  },
+  {
+    "revision": "5a5c0a18ac2f17c752fc",
+    "url": "/cv_project/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3768914cba0649cb503b",
+    "url": "/cv_project/static/css/main.043959c2.chunk.css"
+  },
+  {
+    "revision": "5a5c0a18ac2f17c752fc",
+    "url": "/cv_project/static/js/2.203f5bf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv_project/static/js/2.203f5bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3768914cba0649cb503b",
+    "url": "/cv_project/static/js/main.a6acb6f1.chunk.js"
+  },
+  {
+    "revision": "8a46d0bc7992bf3208db",
+    "url": "/cv_project/static/js/runtime-main.0a9f5415.js"
+  }
+]);
